718f8f327da3f210165f3a0afcca7ab8ac523669
Sönke Hahn <soenkehahn@gmail.com>
2023-11-22 11:40:17 -0500

    Fix formatting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/garn-example-vite-reactts-eslint-prettier/",
+  base: '/garn-example-vite-reactts-eslint-prettier/',
   plugins: [react()],
 });
